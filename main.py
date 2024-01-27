aee7b81b67dde613d3031efebc4c7dbfb9739098
Marcus Johnson <marcus.johnson@ikea.com>
2024-01-28 00:09:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,29 @@ import ST7789
 import time
 import paho.mqtt.client as mqtt
 
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
+from struct import *
 
 
 # topics
 tp_pwr = "zigbee2mqtt/0xe0798dfffe57ec82/set/#"
 tp_t_0 = "SaunaTemp/Temp0/#"
 tp_t_1 = "SaunaTemp/Temp1/#"
-MQTT_TOPICS = [(tp_pwr, 0), (tp_t_0, 0), (tp_t_1, 0)]
+MQTT_TOPICS = [(tp_pwr, 1), (tp_t_0, 1), (tp_t_1, 1)]
+
+key3_disc_topic = "homeassistant/device_automation/sauna_ctrl_key3/action_key3_press/config"
+key3_disc_payload = {
+    "automation_type": "trigger",
+    "type": "action",
+    "subtype": "key3_pressed",
+    "payload": "key3_pressed",
+    "topic": "sauna_ctrl/key3/action",
+    "device": {
+        "name": "sauna_ctrl_key3"
+    }
+}
+key3_trig_topic = "sauna_ctrl/key3/action"
+key3_trig_payload = "key3_pressed"
 
 logging.basicConfig(level=logging.DEBUG)
 # 240x240 display with hardware SPI:
@@ -49,18 +64,29 @@ client.on_message = on_message
 client.connect("fusion", 1883, 60)
 client.loop_start()
 
+client.publish(topic=key3_disc_topic, payload=str(key3_disc_payload), qos=2, retain=False)
+
+
+
 key1 = 0
 p_key1 = 0
+key2 = 0
+p_key2 = 0
+key3 = 0
+p_key3 = 0
 
 try:
     while True:
+        key3 = disp.digital_read(disp.GPIO_KEY3_PIN)
+        key2 = disp.digital_read(disp.GPIO_KEY2_PIN)
         key1 = disp.digital_read(disp.GPIO_KEY1_PIN)
-        if key1 != p_key1:
-            p_key1 = key1
-            if key1 == 1:
-                print("key1 pressed")
+        if key3 != p_key3:
+            p_key3 = key3
+            if key3 == 1:
+                print("key3 pressed")
+                client.publish(topic=key3_trig_topic, payload=str(key3_trig_payload), qos=1, retain=False)
             else:
-                print("key1 released")
+                print("key3 released")
 
 
 except:
